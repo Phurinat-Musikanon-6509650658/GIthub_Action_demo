cd65388974212130420422d43c22cffce51fadac
EC2 Default User <ec2-user@ip-172-31-22-149.ec2.internal>
2024-10-24 05:09:46 +0000

    v1

---
+++ src/__test__/math.test.js
@@ -0,0 +1,13 @@
+const { add, subtract, multiply } = require('../math');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 2 - 1 to equal 1', () => {
+  expect(subtract(2, 1)).toBe(1);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
